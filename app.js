d2a883dddb18efa65631bf214f6d42f62b73fa62
Nick Gupta <manhattan.nick@gmail.com>
2020-10-21 23:18:39 -0400

    updated port code

--- app.js
+++ app.js
@@ -6,7 +6,7 @@ app.get('/', (req, res) => {
 });
 
 //const port = process.env.port || 8081;
-var port = normalizePort(process.env.PORT || '8081');
+var port = normalizePort(process.env.port || '8081');
 
 app.listen(port, () => {
     console.log("What's up?");
