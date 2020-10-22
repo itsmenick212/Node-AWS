871000e788488f34d05c73272e534f0438ec8d75
Nick Gupta <manhattan.nick@gmail.com>
2020-10-22 11:55:35 -0400

    copied app.js to app_copy.js to be able to edit app.js code

---
+++ app_copy.js
@@ -0,0 +1,12 @@
+﻿const express = require('express');
+const app = express();
+
+app.get('/', (req, res) => {
+    res.send("Welcome!");
+});
+
+const port = process.env.port || 8080;
+
+app.listen(port, () => {
+    console.log("What's up?");
+});
